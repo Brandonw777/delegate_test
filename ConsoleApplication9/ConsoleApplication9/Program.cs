13288ff5db611bf6cf3adfec96404a8a9d3872c4
Brandonw777 <brandonw777@yahoo.com>
2015-09-25 10:17:45 -0400

    Start

---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var list = new List<dynamic>
+          {
+               new Func<int> (test1)
+          };
+
+
+            Console.WriteLine("Words...");
+            Console.ReadKey();
+            list.Add(new Func<string, string>(test2));
+            dynamic result = list[0](); // like X(1, 2)
+            
+            Console.WriteLine(result);
+            Console.ReadKey();
+            Console.WriteLine(list[1]("b"));
+            Console.ReadKey();
+
+        }
+
+
+        static int test1()
+        {
+            Console.WriteLine("HI!!!");
+            return 1;
+        }
+        static string test2(string x)
+        {
+            return x;
+        }
+        
+
+    }
+}
